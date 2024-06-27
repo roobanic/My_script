9058a9d6a4fb951ee2f2f6f4a849c52d439f140f
rooban <rooban@sodexis.com>
2024-06-27 15:10:44 +0530

    removed enps0 in month wise and date wise

--- bandwidth_check.sh
+++ bandwidth_check.sh
@@ -14,12 +14,12 @@ echo -e "${RED} ================================================================
 echo
 echo
 echo -e "${BOLDBLUE}		     ********* To find Daily based internet ********* "
-vnstat -d -i enp3s0
+vnstat -d
 echo -e "${RED} ========================================================================${ENDCOLOR}"
 echo
 echo
 echo
 echo -e "${BOLDBLUE} 		  ********* To Find monthly based internet speed ********"
-vnstat -m -i enp3s0
+vnstat -m
 echo -e "${RED} ========================================================================${ENDCOLOR}"
 echo
